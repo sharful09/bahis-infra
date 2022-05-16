46411b22ffc0d1639544a64880a05e14e0146f4d
M Kunde <mikolaj.kundegorski@gmail.com>
2022-05-16 12:32:03 +0600

    working unstable with only one sql file on hand

--- deployment/pgdb/init.sql
+++ deployment/pgdb/init.sql
@@ -1,6 +1,8 @@
 \set ON_ERROR_STOP on
-drop database coredb;
-drop user kobo;
+drop database if exists coredb;
+drop user if exists kobo;
 CREATE USER kobo WITH PASSWORD 'bahis@321';
 create database coredb;
 GRANT ALL PRIVILEGES ON DATABASE coredb to kobo;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
