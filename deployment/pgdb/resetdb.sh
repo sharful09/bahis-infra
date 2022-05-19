55f345eafb590cbd58e4bd16d321d902ebe5f36c
M <mikolaj.kundegorski@gmail.com>
2022-05-19 10:58:04 +0600

    do not tolerate errors

--- deployment/pgdb/resetdb.sh
+++ deployment/pgdb/resetdb.sh
@@ -1,5 +1,6 @@
 #Usage:
 #run with your current database you want o switch to:
 #whichdb=coredb-29-03-2022.sql ./resetdb.sh
-psql -U postgres -h localhost -p 99 -f init.sql
-psql -U postgres -h localhost -p 99 -d coredb -f $whichdb
+set -e
+psql -U postgres -h localhost -p 99 -f init.sql --set ON_ERROR_STOP
+psql -U postgres -h localhost -p 99 -d coredb -f $whichdb --set ON_ERROR_STOP
